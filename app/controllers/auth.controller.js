c5e5939597616431a12f212a7c3521617fb62a84
El Pape <elpapemola@gmail.com>
2020-04-17 19:42:08 +0200

    backend 0.1

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,151 @@
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+const Stat = db.stats;
+
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+exports.signup = (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8)
+  });
+
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    if (req.body.roles) {
+      Role.find(
+        {
+          name: { $in: req.body.roles }
+        },
+        (err, roles) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          user.roles = roles.map(role => role._id);
+          user.save(err => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+            
+            const statUser = new Stat({
+
+              userid: "11",
+              dinero: 10000,
+              fincuenta: new Date(),
+              raza: "humano"
+            });
+
+            statUser.save(err => {
+              if (err) {
+                res.status(500).send({ message: err });
+                return;
+              }
+            }
+            );
+
+            res.send({ message: "User was registered successfully!" });
+          });
+        }
+      );
+    } else {
+      Role.findOne({ name: "user" }, (err, role) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        user.roles = [role._id];
+        user.save(err => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          let fcuennta=new Date();
+          console.log("fcue1"+ fcuennta);
+          let minutos=fcuennta.getMinutes();
+          console.log("minu"+ minutos);
+          fcuennta.setMinutes(minutos+1440);
+          console.log("fcue2"+ fcuennta);
+
+          const statUser = new Stat({
+
+            userid: user._id,
+            dinero: 10000,
+            fincuenta: fcuennta,
+            raza: "humano"
+          });
+          console.log("a");
+          statUser.save(err => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+          }
+          );
+          console.log("b");
+          res.send({ message: "User was registered successfully!" });
+         
+        });
+
+      });
+    }
+  });
+};
+
+exports.signin = (req, res) => {
+  User.findOne({
+    username: req.body.username
+  })
+    .populate("roles", "-__v")
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!"
+        });
+      }
+
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400 // 24 hours
+      });
+
+      var authorities = [];
+
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+      }
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+        accessToken: token
+      });
+    });
+};
